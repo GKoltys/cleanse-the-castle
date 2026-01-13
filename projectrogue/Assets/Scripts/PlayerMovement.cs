0d63d0f2fe45f3afb345009f44dcacc13d44d8d7
GKoltys <gregorykoltys@gmail.com>
2026-01-13 16:00:25 +0000

    Player idle and run animations done

--- projectrogue/Assets/Scripts/PlayerMovement.cs
+++ projectrogue/Assets/Scripts/PlayerMovement.cs
@@ -5,22 +5,42 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
-    public Rigidbody2D rb;
+    private Rigidbody2D rb;
+
     private Vector2 moveInput;
+    private Vector2 lastMoveDirection;
+    private Animator animator;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
+
+        if (moveInput.sqrMagnitude > 0.01f)
+        {
+            lastMoveDirection = moveInput.normalized;
+        }
     }
 
     private void FixedUpdate()
     {
-        Vector2 direction = moveInput.normalized; // normalized prevents faster diagonal movement
+        // Movement
+        Vector2 direction = moveInput.normalized; // "normalized" prevents faster diagonal movement
         rb.linearVelocity = direction * moveSpeed;
+
+        // Animation
+        bool isMoving = moveInput != Vector2.zero;
+        animator.SetBool("IsRunning", isMoving);
+
+        animator.SetFloat("InputX", moveInput.x);
+        animator.SetFloat("InputY", moveInput.y);
+
+        animator.SetFloat("LastInputX", lastMoveDirection.x);
+        animator.SetFloat("LastInputY", lastMoveDirection.y);
     }
 }
