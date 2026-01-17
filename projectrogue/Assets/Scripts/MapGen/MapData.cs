3669d64c235e8fbbead9ac4c69bc26ee30e2ef2f
Max <maksymilian.frackowiak2@mail.dcu.ie>
2026-01-17 19:32:43 +0000

    initial map generation for rendering a basic room

---
+++ projectrogue/Assets/Scripts/MapGen/MapData.cs
@@ -0,0 +1,16 @@
+﻿public class MapData
+{
+
+	public readonly int width;
+	public readonly int height;
+	public TileType[,] tiles;
+
+	public MapData(int w, int h)
+	{
+		width = w; height = h;
+		tiles = new TileType[w, h];
+	}
+
+	public bool InBounds(int x, int y) =>
+		x >= 0 && y >= 0 && x < width && y < height;
+}
