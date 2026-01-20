6f8451382cb8939ff1fa0cc94baccf5971f3b323
GKoltys <gregorykoltys@gmail.com>
2026-01-20 00:50:17 +0000

    Resolutions seem to be working fine, need to fix their dropdown and fix display settings

--- projectrogue/Assets/Scripts/UIScripts/MenuScripts/SettingsMenuController.cs
+++ projectrogue/Assets/Scripts/UIScripts/MenuScripts/SettingsMenuController.cs
@@ -1,10 +1,69 @@
-using Unity.VisualScripting;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
+
+// https://www.youtube.com/watch?v=YOaYQrN1oYQ
 
 public class SettingsMenuController: MonoBehaviour
 {
     public AudioMixer audioMixer;
+
+    public TMP_Dropdown resolutionDropdown;
+
+    private Resolution[] resolutions;
+    private int currentResIndex;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> resList = new();
+
+        currentResIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string res = resolutions[i].width + " x " + resolutions[i].height;
+            resList.Add(res);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(resList);
+        resolutionDropdown.value = currentResIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetDisplayMode(int displayOption)
+    {
+        if (displayOption == 0)
+        {
+            Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+        }
+        else if (displayOption == 1)
+        {
+            Screen.fullScreenMode = FullScreenMode.Windowed;
+        }
+        else if (displayOption == 2)
+        {
+            Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+        }
+
+        Resolution r = resolutions[currentResIndex];
+        Screen.SetResolution(r.width, r.height, Screen.fullScreenMode);
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
 
     public void SetMasterVolume(float volume)
     {
