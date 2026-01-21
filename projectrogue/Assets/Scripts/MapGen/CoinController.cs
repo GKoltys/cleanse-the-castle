16fd014632e8c3ff8d2480b004387d5d4074d049
Max <maksymilian.frackowiak2@mail.dcu.ie>
2026-01-21 00:15:03 +0000

    added coin collect animations and triggers

---
+++ projectrogue/Assets/Scripts/MapGen/CoinController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class CoinController : MonoBehaviour, IMapGenInit
+{
+    private Animator animator;
+    private Collider2D col;
+    private MapGenerator dungeon;
+    private bool collected = false;
+
+    public void Init(MapGenerator controller)
+    {
+        dungeon = controller;
+        animator = GetComponent<Animator>();
+        col = GetComponent<Collider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (dungeon == null) return;
+
+        if (!other.CompareTag("Player")) return;
+
+        collected = true;
+        if (col) col.enabled = false;
+
+        if (animator != null && collected)
+        {
+            // animation transitions - https://docs.unity3d.com/6000.3/Documentation/Manual/class-Transition.html
+            animator.SetTrigger("CoinGet");
+        }
+
+    }
+
+    public void Despawn()
+    {
+        // destory for now, if it affects performance move to object pooling?
+        Destroy(gameObject);
+    }
+}
