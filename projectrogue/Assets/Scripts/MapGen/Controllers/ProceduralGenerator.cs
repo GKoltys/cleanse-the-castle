aa3b950c00263778f01fecc0061bbed80698a0e0
Max <maksymilian.frackowiak2@mail.dcu.ie>
2026-01-28 23:59:43 +0000

    rebase and start procedural generation

---
+++ projectrogue/Assets/Scripts/MapGen/Controllers/ProceduralGenerator.cs
@@ -0,0 +1,29 @@
+﻿//using System.Collections.Generic;
+//using UnityEngine;
+
+//// procedural generation with BSP algorithm
+//// https://www.roguebasin.com/index.php/Basic_BSP_Dungeon_generation
+//public class ProceduralGenerator : MonoBehaviour
+//{
+//    public MapData GenerateFloor(int w, int h, int pad)
+//    {
+//        var map = new MapData(w, h);
+
+//        // create a room with tiles set to wall
+//        for (int x = xMin; x <= xMax; x++)
+//            for (int y = yMin; y <= yMax; y++)
+//                map.tiles[x, y] = TileType.Wall;
+
+
+//        split recursively until each sub - dungeon is approx size of a room
+
+
+//        create random - size room in each sub-dungeon
+
+
+//         connect each room with corridors
+//     }
+
+//}
+
+   
