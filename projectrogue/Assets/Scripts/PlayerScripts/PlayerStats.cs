e18624174da81cedc523312c1afee73dd8365f93
GKoltys <gregorykoltys@gmail.com>
2026-02-10 22:37:35 +0000

    added player attack animations

---
+++ projectrogue/Assets/Scripts/PlayerScripts/PlayerStats.cs
@@ -0,0 +1,21 @@
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    private float maxHealth;
+    private float health;
+    private float damage; // Needs to be replaced with a 'Weapon' class
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
