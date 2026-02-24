966e2fb791a7496c6526e48e9d67006a1649b744
Max <maksymilian.frackowiak2@mail.dcu.ie>
2026-02-24 15:52:29 +0000

    setup NPC dialogue script

---
+++ projectrogue/Assets/Scripts/NPCScripts/NPC.cs
@@ -0,0 +1,16 @@
+//using UnityEngine;
+//using UnityEngine.UI;
+//using TMPPro;
+
+//public class NPC : MonoBehaviour
+//{
+//    public NPCDialogue dialogueData;
+//    public GameObject dialoguePanel;
+//    public TMP_Text dialogueText, nameText;
+//    public Image portraitImage;
+
+//    private int dialogueIndex;
+//    private bool isTyping, isDialogueActive;
+
+//    }
+//}
---
+++ projectrogue/Assets/Scripts/NPCScripts/NPCDialogue.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="NewNPCDialogue", menuName = "NPC Dialogue")]
+public class NPCDialogue : ScriptableObject
+{
+	public string npcName;
+	public Sprite npcPortrait;
+	public string[] dialogueLines;
+	public float typingSpeed = 0.05f;
+
+}
