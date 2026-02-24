08d3d7fdfe383b780e1833276c77c2450308b3b3
Max <maksymilian.frackowiak2@mail.dcu.ie>
2026-02-24 21:41:20 +0000

    start on interaction detection

--- projectrogue/Assets/Scripts/NPCScripts/NPC.cs
+++ projectrogue/Assets/Scripts/NPCScripts/NPC.cs
@@ -1,16 +1,90 @@
-//using UnityEngine;
-//using UnityEngine.UI;
-//using TMPPro;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
 
-//public class NPC : MonoBehaviour
-//{
-//    public NPCDialogue dialogueData;
-//    public GameObject dialoguePanel;
-//    public TMP_Text dialogueText, nameText;
-//    public Image portraitImage;
+public class NPC : MonoBehaviour, IInteractable
+{
+    public NPCDialogue dialogueData;
+    public GameObject dialoguePanel;
+    public TMP_Text dialogueText, nameText;
+    public Image portraitImage;
 
-//    private int dialogueIndex;
-//    private bool isTyping, isDialogueActive;
+    private int dialogueIndex;
+    private bool isTyping, isDialogueActive;
 
-//    }
-//}
+    public void Interact()
+    {
+        if(dialogueData == null)
+        {
+            return;
+        }
+        if (isDialogueActive)
+        {
+            NextLine();
+        }
+        else
+        {
+            StartDialogue();
+        }
+    }
+
+    public bool CanInteract()
+    {
+        return !isDialogueActive;
+    }
+
+    void StartDialogue()
+    {
+        isDialogueActive = true;
+        dialogueIndex = 0;
+
+        nameText.SetText(dialogueData.name);
+        portraitImage.sprite = dialogueData.npcPortrait;
+
+        dialoguePanel.SetActive(true);
+        StartCoroutine(Typeline());
+
+    }
+
+    void NextLine()
+    {
+        if(isTyping)
+        {
+            StopAllCoroutines();
+            dialogueText.SetText(dialogueData.dialogueLines[dialogueIndex]);
+            isTyping = false;
+        }
+        else if(++dialogueIndex < dialogueData.dialogueLines.Length)
+        {
+            StartCoroutine(Typeline());
+        }
+        else
+        {
+            EndDialogue();
+        }
+    }
+
+    IEnumerator Typeline()
+    {
+        isTyping = true;
+        dialogueText.SetText("");
+
+        foreach(char letter in dialogueData.dialogueLines[dialogueIndex])
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(dialogueData.typingSpeed);
+        }
+
+        isTyping = false;
+    }
+
+    public void EndDialogue()
+    {
+        StopAllCoroutines();
+        isDialogueActive = false;
+        dialogueText.SetText("");
+        dialoguePanel.SetActive(false);
+    }
+
+}
---
+++ projectrogue/Assets/Scripts/PlayerScripts/InteractionDetector.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InteractionDetector : MonoBehaviour
+{
+	private IInteractable interactableInRange = null;
+	void Start()
+	{
+		// set detection icon to false
+	}
+
+	public void OnInteract(InputAction.CallbackContext ctx)
+	{
+		if (ctx.performed)
+		{
+			interactableInRange = interactable;
+			interactableInRange?.Interact();
+		}
+	} 
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if(collision.TryGetComponent(out IInteractable interactable) && interactable.CanInteract())
+		{
+			interactableInRange = interactable;
+            // set detection icon to true
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out IInteractable interactable) && interactable == interactableInRange)
+        {
+			interactableInRange = null;
+            // set detection icon to false
+        }
+    }
+}
