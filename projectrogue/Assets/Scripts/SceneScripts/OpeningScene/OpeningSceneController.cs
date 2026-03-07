37b0472e40898dfd1053acca7619df7c61d1244f
GKoltys <gregorykoltys@gmail.com>
2026-03-07 18:00:58 +0000

    Cutscene can now be skipped

--- projectrogue/Assets/Scripts/SceneScripts/OpeningScene/OpeningSceneController.cs
+++ projectrogue/Assets/Scripts/SceneScripts/OpeningScene/OpeningSceneController.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
 
-// https://docs.unity3d.com/ScriptReference/Playables.PlayableDirector-stopped.html
 public class OpeningSceneController : MonoBehaviour
 {
+    // https://docs.unity3d.com/ScriptReference/Playables.PlayableDirector-stopped.html
     [SerializeField] private PlayableDirector director;
 
     void OnEnable()
@@ -22,5 +23,16 @@ public class OpeningSceneController : MonoBehaviour
         Debug.Log("cutscene is over");
         SaveController.Instance.RequestLoad();
         SceneManager.LoadSceneAsync(2);
+    }
+
+    // To skip cutscene
+    private void Update()
+    {
+        if (Keyboard.current != null && Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            Debug.Log("cutscene was skipped");
+            SaveController.Instance.RequestLoad();
+            SceneManager.LoadSceneAsync(2);
+        }
     }
 }
