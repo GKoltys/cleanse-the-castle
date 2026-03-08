4b855dc01445b1617ab53ee58d43502600f47e5f
GKoltys <gregorykoltys@gmail.com>
2026-03-08 13:17:44 +0000

    Blue slime enemy prefab created with all components added

---
+++ projectrogue/Assets/Scripts/EnemyScripts/SlimeScripts/SlimeAttack.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class Slime : EnemyAttack
+{
+    // Here we can override any basic enemy attack logic
+    // but still inherit all of its functionality
+}
---
+++ projectrogue/Assets/Scripts/EnemyScripts/SlimeScripts/SlimeEnemy.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class SlimeEnemy : EnemyBase
+{
+    // Here we can override any basic enemy logic
+    // but still inherit all of its functionality
+}
