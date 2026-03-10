2163e4cd36784320d4a8d5aae585c85d9e5a6c70
GKoltys <gregorykoltys@gmail.com>
2026-03-10 12:47:01 +0000

    Added a drop chance field to all items

--- projectrogue/Assets/Scripts/ConsumablesScripts/ConsumableItemData.cs
+++ projectrogue/Assets/Scripts/ConsumablesScripts/ConsumableItemData.cs
@@ -6,5 +6,6 @@ public class ConsumableItemData : ScriptableObject
     public string itemName;
     public Sprite icon;
     public string description;
+    [Range(0, 1)] public float dropChance;
     public ConsumableEffect effect;
 }
