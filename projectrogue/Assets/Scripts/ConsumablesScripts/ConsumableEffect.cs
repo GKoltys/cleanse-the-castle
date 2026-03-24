f10891fa9274ea9b65f6f31bab7c091259413111
Max <maksymilian.frackowiak2@mail.dcu.ie>
2026-03-24 22:19:53 +0000

    fix remove method to virtual

--- projectrogue/Assets/Scripts/ConsumablesScripts/ConsumableEffect.cs
+++ projectrogue/Assets/Scripts/ConsumablesScripts/ConsumableEffect.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public abstract class ConsumableEffect : ScriptableObject
 {
     public abstract void Apply(PlayerApplyEffect player);
-    public abstract void Remove(PlayerApplyEffect player);
+    public virtual void Remove(PlayerApplyEffect player)
+    {
+        // option override used for relic type consumables
+    }
     public abstract float GetStatChangeValue();
 }
