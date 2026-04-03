95d3fdbdc47dab70c8022ab53b80f9366e91fc4a
GKoltys <gregorykoltys@gmail.com>
2026-04-03 20:37:06 +0100

    blue knights and their loot tables are ready

--- projectrogue/Assets/Scripts/EnemyScripts/EnemyLootDropper.cs
+++ projectrogue/Assets/Scripts/EnemyScripts/EnemyLootDropper.cs
@@ -10,7 +10,7 @@ public class EnemyLootDropper : MonoBehaviour
 
         GameObject itemToDrop = dropTable.GetRandomLootItem();
         if (itemToDrop == null) return;
-
+        Debug.Log("Should be dropping " + itemToDrop.name);
         Instantiate(itemToDrop, transform.position, Quaternion.identity);
     }
 }
