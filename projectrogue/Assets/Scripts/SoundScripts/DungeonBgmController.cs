6a90dd1389094065b126be2fb30eae87b5d37e39
GKoltys <gregorykoltys@gmail.com>
2026-04-05 16:13:15 +0100

    New background music working all throughout the project

---
+++ projectrogue/Assets/Scripts/SoundScripts/DungeonBgmController.cs
@@ -0,0 +1,54 @@
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DungeonBgmController : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    [SerializeField] private AudioClip dungeon;
+    [SerializeField] private AudioClip bossBattle;
+    [SerializeField] private AudioClip finalBattle;
+
+    public static DungeonBgmController Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void ChangeMusic(int floor)
+    {
+        AudioClip newClip;
+
+        switch (floor)
+        {
+            case 10:
+            case 20:
+                newClip = bossBattle;
+                break;
+
+            case 30:
+                newClip = finalBattle;
+                break;
+
+            default:
+                newClip = dungeon;
+                break;
+        }
+
+        if (audioSource.clip == newClip) return;
+
+        audioSource.clip = newClip;
+        audioSource.Play();
+    }
+}
